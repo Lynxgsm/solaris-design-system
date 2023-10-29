5e6ac4332da62e0ce20f093b3da9cf7a0250dee4
AILI FIDA Aliotti Christino <geko@MacBook-Pro-de-AILI.local>
2023-10-29 19:53:55 +0100

    added cards

---
+++ apps/react-storybook/src/App.tsx
@@ -0,0 +1,9 @@
+import { SolarisCard, SolarisCards } from "solaris-react";
+
+export default function App() {
+  return (
+    <SolarisCards style={{ backgroundColor: "red" }}>
+      <SolarisCard>Hello World</SolarisCard>
+    </SolarisCards>
+  );
+}
