34ec8a44a7f892b87c1caf14bd5608ce538c4be7
AILI Fida Aliotti Christino <aili.fida.ac@gmail.com>
2023-11-03 17:43:34 +0100

    fixed tab to horizontally scroll

--- apps/react-storybook/src/App.tsx
+++ apps/react-storybook/src/App.tsx
@@ -1,9 +1,33 @@
-import { SolarisLabel } from "solaris-react";
+import {
+  SolarisPill,
+  SolarisTab,
+  SolarisTabPanel,
+  SolarisTabs,
+} from "solaris-react";
 
 export default function App() {
   return (
-    <SolarisLabel icon="arrow-up" variant="default">
-      Hello World
-    </SolarisLabel>
+    <SolarisTabs>
+      <SolarisTab slot="tab" tab="tab1">
+        <SolarisPill label="Envoyé" count={4}></SolarisPill>
+      </SolarisTab>
+      <SolarisTab slot="tab" tab="tab2">
+        <SolarisPill label="Envoyé" count={4}></SolarisPill>
+      </SolarisTab>
+      <SolarisTab slot="tab" tab="tab3">
+        <SolarisPill label="Envoyé" count={4}></SolarisPill>
+      </SolarisTab>
+      <div>
+        <SolarisTabPanel slot="tab-panel" tab="tab1" active>
+          Content 1
+        </SolarisTabPanel>
+        <SolarisTabPanel slot="tab-panel" tab="tab2">
+          Content 2
+        </SolarisTabPanel>
+        <SolarisTabPanel slot="tab-panel" tab="tab3">
+          Content 3
+        </SolarisTabPanel>
+      </div>
+    </SolarisTabs>
   );
 }
