e710bd70cf0aa15420b2e4ecf8a8c620823b0eb7
AILI Fida Aliotti Christino <aili.fida.ac@gmail.com>
2023-11-03 17:45:33 +0100

    example with tab utilization

--- apps/react-storybook/src/App.tsx
+++ apps/react-storybook/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import {
   SolarisPill,
   SolarisTab,
@@ -5,29 +6,87 @@ import {
   SolarisTabs,
 } from "solaris-react";
 
-export default function App() {
+const data = [
+  {
+    title: "Envoyés",
+    items: [
+      {
+        title: "Title 1",
+      },
+      {
+        title: "Title 2",
+      },
+      {
+        title: "Title 3",
+      },
+    ],
+  },
+  {
+    title: "Suspendu",
+    items: [
+      {
+        title: "Title 1",
+      },
+    ],
+  },
+  {
+    title: "En cours",
+    items: [
+      {
+        title: "Title 1",
+      },
+      {
+        title: "Title 2",
+      },
+    ],
+  },
+  {
+    title: "Terminé",
+    items: [],
+  },
+];
+
+export default function ContractContent() {
+  const [selectedTab, setSelectedTab] = useState("tab1");
+  const ref = useRef<HTMLSolarisTabsElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener("tabSelected", (event) => {
+        const customEvent = event as CustomEvent<string>;
+        setSelectedTab(customEvent.detail);
+      });
+    }
+  }, []);
+
   return (
-    <SolarisTabs>
-      <SolarisTab slot="tab" tab="tab1">
-        <SolarisPill label="Envoyé" count={4}></SolarisPill>
-      </SolarisTab>
-      <SolarisTab slot="tab" tab="tab2">
-        <SolarisPill label="Envoyé" count={4}></SolarisPill>
-      </SolarisTab>
-      <SolarisTab slot="tab" tab="tab3">
-        <SolarisPill label="Envoyé" count={4}></SolarisPill>
-      </SolarisTab>
-      <div>
-        <SolarisTabPanel slot="tab-panel" tab="tab1" active>
-          Content 1
-        </SolarisTabPanel>
-        <SolarisTabPanel slot="tab-panel" tab="tab2">
-          Content 2
-        </SolarisTabPanel>
-        <SolarisTabPanel slot="tab-panel" tab="tab3">
-          Content 3
-        </SolarisTabPanel>
-      </div>
+    <SolarisTabs ref={ref}>
+      {data.map((item, index) => {
+        return (
+          <>
+            <SolarisTab key={`tab${index}`} slot="tab" tab={`tab${index + 1}`}>
+              <SolarisPill
+                label={item.title}
+                count={item.items.length}
+                active={selectedTab === `tab${index + 1}`}
+              ></SolarisPill>
+            </SolarisTab>
+            <SolarisTabPanel
+              active={selectedTab === `tab${index + 1}`}
+              slot="tab-panel"
+              tab={`tab${index + 1}`}
+            >
+              {item.title === "Suspendu" && <p>Hello World</p>}
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Dolorum illum obcaecati earum aspernatur quisquam nemo. Omnis
+                voluptate repellat harum repudiandae aliquam! Nostrum laborum
+                neque esse nesciunt porro, optio obcaecati hic.
+              </p>
+            </SolarisTabPanel>
+          </>
+        );
+      })}
     </SolarisTabs>
   );
 }
